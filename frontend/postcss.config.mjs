a6e8a2a9b434abf194318d8cd9369ae3a2896f51
lxbachit03 <lxbachit03.working@gmail.com>
2025-09-02 01:53:14 +0700

    feat: add postcss-preset-env to use modern CSS features for compatibility with specified browsers

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,12 +1,27 @@
 import autoprefixer from 'autoprefixer'
 import cssnano from 'cssnano'
+import postcssPresetEnv from 'postcss-preset-env'
+
+const plugins = [
+  '@tailwindcss/postcss',
+  autoprefixer,
+  postcssPresetEnv({
+    stage: 3,
+    features: {
+      'nesting-rules': true,
+      'custom-properties': true,
+      'custom-media-queries': true,
+      'logical-properties-and-values': true,
+    },
+  }),
+]
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(cssnano({ preset: 'default' }))
+}
 
 const config = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer,
-    ...(process.env.NODE_ENV === 'production' && cssnano({ preset: 'default' })),
-  },
+  plugins,
 }
 
 export default config
