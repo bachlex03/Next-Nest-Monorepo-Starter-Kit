f1caec25e7d1c830b761e482a662499f9273b768
lxbachit03 <lxbachit03.working@gmail.com>
2025-09-02 14:12:23 +0700

    chore: add sass dependency

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import Button from '~/components/SampleComponent'
+import Button from '~/components/app/SampleComponent'
 
 export default function Home() {
   return <h1 className='mr-5 w-5 bg-white px-3 pt-5 text-2xl font-bold underline'>Hello world!</h1>
